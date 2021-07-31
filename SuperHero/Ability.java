9f41aa97edb2c6265420cdbdfd959a9483ef2be6
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2021-07-31 23:33:13 +0700

    Add files via upload

---
+++ SuperHero/Ability.java
@@ -0,0 +1,9 @@
+interface Ability{
+
+    public void moving(String moving);
+
+    public void weapon(String weapon);
+
+    public void jumping(String jump);
+
+}
---
+++ SuperHero/Ironman.java
@@ -0,0 +1,18 @@
+class Ironman extends SuperHero implements Ability{
+
+    public void skill(String skill){
+        System.out.println("Skill = " + skill);
+    }
+
+    public void moving(String moving){
+        
+    }
+
+    public void weapon(String weapon){
+
+    }
+
+    public void jumping(String jump){
+
+    }
+}
